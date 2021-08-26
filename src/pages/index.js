a2e8a072ecff444220c6feac248af72fda3a1c8f
Israel Cena <israelsjm@gmail.com>
2021-08-26 20:53:18 -0300

    organize folders and remove default code

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <section>
+      <Head>
+        <title>Coffee Template</title>
+        <meta name="description" content="A open source template" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Hello</h1>
+      </main>
+      <footer>
+        <p>Bye</p>
+      </footer>
+    </section>
+  )
+}
