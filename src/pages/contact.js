01a53694c59920e126093bbc99e0e35d35dfb923
Israel Cena <israelsjm@gmail.com>
2021-08-26 21:01:37 -0300

    create contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+        voluptatibus, doloremque quisquam, dolore, nisi, quibusdam repudiandae,
+        eum, quisquam quis, veritatis doloribus.
+      </p>
+    </div>
+  )
+}
