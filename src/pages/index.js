c166e1f522f9a1fcfa61243a7f47840c12b39021
Israel Cena <israelsjm@gmail.com>
2021-08-26 21:25:10 -0300

    include images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import Head from '../components/Head'
 import Navbar from '../components/Navbar'
 
@@ -9,7 +11,26 @@ export default function Home() {
         <Navbar />
       </header>
       <main>
-        <h1>Hello</h1>
+        <div>
+          <h2>
+            Esse café não é apenas um café <br />
+            Esse é o <span>Café Gostoso</span>
+          </h2>
+          <p>
+            Feito com todo o carinho e dedicação, nossa equipe de cientistas
+            está trabalhando para criar o melhor café do mundo. Contudo, estamos
+            no caminho devido ao grande número de pedidos.
+          </p>
+          <Link href="/cafe">Leia Mais</Link>
+        </div>
+        <div>
+          <Image
+            src="/images/coffee1.png"
+            alt="Coffee 1"
+            width={500}
+            height={500}
+          />
+        </div>
       </main>
       <footer>
         <p>Bye</p>
