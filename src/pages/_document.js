4add9af37693129d699e8622a120546e6be44760
Israel Cena <israelsjm@gmail.com>
2021-08-29 20:24:29 -0300

    fix warning at crossOrigin

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,11 @@ class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap"
             rel="stylesheet"
