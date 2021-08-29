60fd6e58766e50cc0f47883dc09b4708473d00ef
Israel Cena <israelsjm@gmail.com>
2021-08-28 23:00:53 -0300

    add nav bar component with styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,34 @@
 import Link from 'next/link'
+import Image from 'next/image'
+
+import { useRef, useCallback } from 'react'
+
+import navLogo from '../../public/images/logo.png'
+import { FaBars } from 'react-icons/fa'
+
 import Styles from '../styles/Navbar.module.scss'
 
 export default function Navbar() {
+  const navRef = useRef(null)
+  const handleClick = useCallback(() => {
+    navRef.current.classList.toggle(Styles.active)
+  }, [navRef])
+
   return (
     <nav className={Styles.navbar}>
-      <Link href="/">Café Gostoso</Link>
-      <ul>
+      <Link href="/">
+        <div className={Styles.navbarLogo}>
+          <Image
+            src={navLogo}
+            alt="Logo"
+            layout="responsive"
+            width={240}
+            height={139}
+            className={Styles.navbarLogoImage}
+          />
+        </div>
+      </Link>
+      <ul className={Styles.navbarNav} ref={navRef}>
         <li>
           <Link href="/">Home</Link>
         </li>
@@ -19,6 +42,7 @@ export default function Navbar() {
           <Link href="/">Contato</Link>
         </li>
       </ul>
+      <FaBars onClick={handleClick} className={Styles.navbarIcon} />
     </nav>
   )
 }
